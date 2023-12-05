52833cfac0276237e566c1b8f5c5ac1422a3d344
Nick Strauch <njs35@duke.edu>
2023-12-05 16:30:22 -0500

    Removed old sqlite db connection

--- src/app/routes.py
+++ src/app/routes.py
@@ -41,7 +41,6 @@ def get_products():
         }
         formatted_products.append(formatted_product)
 
-    # Wrap the list in another dictionary under the key 'products'
     return {'products': formatted_products}
 
 
@@ -54,27 +53,9 @@ def create_order():
         db.session.commit()
         print(f"New order! {new_order.id}")
         return jsonify({'message': 'Order created', 'order_id': new_order.id}), 201
+    
     elif request.method == 'GET':
-        
         data = get_products()
-        # data = {
-        #     'products': [
-        #         {
-        #             'product_name': 'Test Product',
-        #             'price': 10.45,
-        #             'description': "Test description....",
-        #             'image': 'https://images.unsplash.com/photo-1575378969004-967d6cfc4114?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        #             'num_in_stock': 1000
-        #         },
-        #         {
-        #             'product_name': 'Test Product 2',
-        #             'price': 0.01,
-        #             'description': "Test description 2....",
-        #             'image': 'https://media.licdn.com/dms/image/D5603AQEkhe4QiNksCA/profile-displayphoto-shrink_800_800/0/1698067367739?e=1707350400&v=beta&t=iez-cGAVpOWLKIGuNesIA1QWivYpmSzZJ7zXgLIx3aY',
-        #             'num_in_stock': 1
-        #         }
-        #     ]
-        # }
         return render_template('index.html', result=data)
 
 
--- src/init_db.py
+++
@@ -1,5 +0,0 @@
-from app import app, db
-from app.datapipe import Order
-
-with app.app_context():
-    db.create_all()
