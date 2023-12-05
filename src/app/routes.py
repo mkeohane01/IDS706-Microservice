4c9dd1ec13cd4b5cf97ccdb893acc8b287a05384
Nick Strauch <njs35@duke.edu>
2023-12-05 14:08:38 -0500

    Got Prodcuts dynamically rendering

--- src/app/routes.py
+++ src/app/routes.py
@@ -13,7 +13,26 @@ def create_order():
         print(f"New order! {new_order.id}")
         return jsonify({'message': 'Order created', 'order_id': new_order.id}), 201
     elif request.method == 'GET':
-        return render_template('index.html')
+
+        data = {
+            'products': [
+                {
+                    'product_name': 'Test Product',
+                    'price': 10.45,
+                    'description': "Test description....",
+                    'image': 'https://images.unsplash.com/photo-1575378969004-967d6cfc4114?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                    'num_in_stock': 1000
+                },
+                {
+                    'product_name': 'Test Product 2',
+                    'price': 0.01,
+                    'description': "Test description 2....",
+                    'image': 'https://media.licdn.com/dms/image/D5603AQEkhe4QiNksCA/profile-displayphoto-shrink_800_800/0/1698067367739?e=1707350400&v=beta&t=iez-cGAVpOWLKIGuNesIA1QWivYpmSzZJ7zXgLIx3aY',
+                    'num_in_stock': 1
+                }
+            ]
+        }
+        return render_template('index.html', result=data)
 
 
 @app.route('/orders/<int:order_id>', methods=['GET'])
@@ -28,12 +47,47 @@ def get_order(order_id):
         'customer_name': 'Test Customer',
         'address': '123 Test Street, NC 27502'
     }
-    return render_template('view_order.html', result={
-                                                'order_id': order_id,
-                                                'product_name': order["product_name"],
-                                                'quantity': order["quantity"],
-                                                'price': order["price"],
-                                                'customer_name': order["customer_name"],
-                                                'address': order["address"],
-                                                'popular_products': ["a", "b", "c"]
-                                            })
+
+    order_data = {
+                'order_id': order_id,
+                'product_name': order["product_name"],
+                'quantity': order["quantity"],
+                'total_price': order["total_price"],
+                'customer_name': order["customer_name"],
+                'address': order["address"],
+                'popular_products': ["a", "b", "c"]
+            }
+
+    return render_template('view_order.html', result=order_data)
+
+
+@app.route('/get_product', methods=['GET'])
+def get_product():
+    product_name = request.args.get('product_name')
+    print(f"Product: {product_name}")
+    data = {
+            'products': [
+                {
+                    'product_name': 'Test Product',
+                    'price': 10.45,
+                    'description': "Test description....",
+                    'image': 'https://images.unsplash.com/photo-1575378969004-967d6cfc4114?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                    'num_in_stock': 1000
+                },
+                {
+                    'product_name': 'Test Product 2',
+                    'price': 0.01,
+                    'description': "Test description 2....",
+                    'image': 'https://media.licdn.com/dms/image/D5603AQEkhe4QiNksCA/profile-displayphoto-shrink_800_800/0/1698067367739?e=1707350400&v=beta&t=iez-cGAVpOWLKIGuNesIA1QWivYpmSzZJ7zXgLIx3aY',
+                    'num_in_stock': 1
+                }
+            ]
+        }
+    
+    for product in data["products"]:
+        if product["product_name"] == product_name:
+            return jsonify(product)
+        
+    return jsonify({'message': 'Product not found'}), 404
+
+    
