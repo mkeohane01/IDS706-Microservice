c3db6c0c1e2f0aa0a3a78c29ce5688b66c62adf0
nicholasconterno <nec21@duke.edu>
2023-12-05 20:39:07 -0500

    push dockerfile

--- src/main.py
+++ src/main.py
@@ -4,4 +4,4 @@ import logging
 logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5001)
